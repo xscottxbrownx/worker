4cb4ccb7c4606052956a8a43e4bf9f54e655d1c5
veganedge <scottxbrown@yahoo.com>
2025-01-29 09:09:22 -0800

    verbiage changes to the bot installed DM to server owner

--- bot/listeners/guildcreate.go
+++ bot/listeners/guildcreate.go
@@ -73,11 +73,11 @@ func sendIntroMessage(ctx context.Context, worker *worker.Context, guild guild.G
 
 	msg := embed.NewEmbed().
 		SetTitle("Tickets").
-		SetDescription("Thank you for inviting Tickets to your server! Below is a quick guide on setting the bot up, please don't hesitate to contact us in our [support server](https://discord.gg/ticketsbot) if you need any assistance!").
+		SetDescription("Thank you for inviting Tickets to your server! Below is a quick guide on setting up the bot, please don't hesitate to contact us in our [support server](https://discord.gg/ticketsbot) if you need any assistance!").
 		SetColor(customisation.GetColourOrDefault(ctx, guild.Id, customisation.Green)).
-		AddField("Setup", "You can setup the bot using `/setup`, or you can use the [dashboard](https://dashboard.ticketsbot.cloud) which has additional options", false).
-		AddField("Reaction Panels", fmt.Sprintf("Reaction panels are a commonly used feature of the bot. You can read about them [here](https://ticketsbot.cloud/panels), or create one on [the dashboard](https://dashboard.ticketsbot.cloud/manage/%d/panels)", guild.Id), false).
-		AddField("Adding Staff", "To make staff able to answer tickets, you must let the bot know about them first. You can do this through\n`/addsupport [@User / @Role]` and `/addadmin [@User / @Role]`. Administrators can change the settings of the bot and access the dashboard.", false).
+		AddField("Setup", "You can setup the bot using `/setup`, or you can use the [web dashboard](https://dashboard.ticketsbot.cloud) which has additional options", false).
+		AddField("Ticket Panels", fmt.Sprintf("Ticket panels are a commonly used feature of the bot. You can read about them [here](https://ticketsbot.cloud/panels), or create one on the [web dashboard](https://dashboard.ticketsbot.cloud/manage/%d/panels)", guild.Id), false).
+		AddField("Adding Staff", "To allow staff to answer tickets, you must let the bot know about them first. You can do this through\n`/addsupport [@User / @Role]` and `/addadmin [@User / @Role]`. While both Support and Admin can access the dashboard, Bot Admins can change the settings of the bot.", false).
 		AddField("Tags", "Tags are predefined tickets of text which you can access through a simple command. You can learn more about them [here](https://ticketsbot.cloud/tags).", false).
 		AddField("Claiming", "Tickets can be claimed by your staff such that other staff members cannot also reply to the ticket. You can learn more about claiming [here](https://ticketsbot.cloud/claiming).", false).
 		AddField("Additional Support", "If you are still confused, we welcome you to our [support server](https://discord.gg/ticketsbot). Cheers.", false)
